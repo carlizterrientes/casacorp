faf95133a8d44c5f3256f8a4c88240b1298456ca
james-n-m <jamesm0025@gmail.com>
2020-09-01 18:46:06 -0400

    updated image size and made form take up less columns on large screen sizes

--- js/app.js
+++ js/app.js
@@ -3,8 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const tabs = document.querySelectorAll('.nav-item');
 
-    console.log(tabs);
-
     tabs.forEach((tab) => {
         tab.addEventListener("click", () => {
             let current = document.querySelector(".active");
